2299c5f78001dcd1d517723edc33a93cbd2aaed5
Curro <kkuetx@gmail.com>
2022-05-19 17:48:29 +0200

    primer commit

---
+++ src/test/java/com/funfit/funfit/FunFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funfit.funfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
